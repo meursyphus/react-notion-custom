88028511d9ec8e3669b221e49ee122e201518cdc
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-01 16:10:14 +0900

    docs: more storybook

---
+++ packages/story/src/stories/code/getCodeExampleJson.ts
@@ -0,0 +1,31 @@
+export function getCodeExampleJson(code: string, language: string) {
+  return [
+    {
+      object: "block",
+      type: "code",
+      code: {
+        caption: [],
+        language: language,
+        rich_text: [
+          {
+            type: "text",
+            text: {
+              content: code,
+              link: null,
+            },
+            annotations: {
+              bold: false,
+              italic: false,
+              strikethrough: false,
+              underline: false,
+              code: false,
+              color: "default",
+            },
+            plain_text: code,
+            href: null,
+          },
+        ],
+      },
+    },
+  ] as any;
+}
