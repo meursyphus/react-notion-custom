324aba0e8f7459dcbb367e6d32db25dbcd4d47fc
anonymousrecords <a.Records2022@gmail.com>
2024-10-06 22:47:09 +0900

    feat: implement home page

--- packages/docs/app/[lang]/page.tsx
+++ packages/docs/app/[lang]/page.tsx
@@ -1,11 +1,9 @@
+import MouseImageEffect from "@/components/effects/mouse-image-effect";
+
 export default function Home({
   params: { lang },
 }: {
   params: { lang: string };
 }) {
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold mb-4">react-notion-custom Docs</h1>
-    </div>
-  );
+  return <MouseImageEffect />;
 }
