3968931431e1f1ccdfc5b15626d86b30237202d1
anonymousrecords <a.Records2022@gmail.com>
2024-10-06 22:45:27 +0900

    style: add logo animation effect

--- packages/docs/components/navigation.tsx
+++ packages/docs/components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Link from "next/link";
 
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import Image from "next/image";
 import LanguageSelector from "./language-selector";
@@ -10,15 +10,24 @@ export default function Navigation() {
   const lang = (params.lang as string) || "en";
 
   return (
-    <nav className="bg-white dark:bg-gray-800 shadow">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="bg-white dark:bg-black border-b-2 border-gray-100 ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-36">
         <div className="flex justify-between h-16">
           <Link
             href={`/${lang}`}
             className="flex-shrink-0 flex gap-2 items-center"
           >
-            <Image src="/logo.svg" alt="Logo" width={40} height={40} />
-            <p>react-notion-custom</p>
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={40}
+              height={40}
+              className="logo-image"
+              style={{ cursor: "pointer" }}
+            />
+            <p className="dark:text-white hidden sm:block">
+              react-notion-custom
+            </p>
           </Link>
 
           <div className="flex gap-5 items-center ml-auto">
@@ -26,9 +35,11 @@ export default function Navigation() {
               href={`/${lang}/guide/getting-started/introduction`}
               className="text-black dark:text-white items-center px-1 pt-1 text-sm font-medium"
             >
-              Guide Documentation
+              Docs
             </Link>
-            <LanguageSelector />
+            <div className="hidden sm:block">
+              <LanguageSelector />
+            </div>
           </div>
         </div>
       </div>
