b0a1e1c9e488928a61b57a2f8ca8904abd0f3251
anonymousrecords <a.Records2022@gmail.com>
2024-10-07 21:32:59 +0900

    feat: add redirect (/ -> /en)

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
