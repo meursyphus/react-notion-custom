232c26b408849a652015092d981d9f74de5feb1a
anonymousrecords <a.Records2022@gmail.com>
2024-10-10 20:06:29 +0900

    refacter: implement toc

--- packages/docs/app/[lang]/guide/[group]/[slug]/page.tsx
+++ packages/docs/app/[lang]/guide/[group]/[slug]/page.tsx
@@ -1,16 +1,6 @@
-import ReactMarkdown from "react-markdown";
-import rehypeRaw from "rehype-raw";
-import rehypeHighlight from "rehype-highlight";
 import { getAllDocuments, getDocumentBySlug } from "@/lib/mdx";
-import {
-  DynamicLayout,
-  MobileSidebar,
-  NavigationButton,
-  Sidebar,
-  TOC,
-} from "@/components";
 import { Metadata } from "next";
-import { generateTOC } from "@/lib/generateTOC";
+import GuidePage from "./guide-page";
 
 interface GuidePageProps {
   params: {
@@ -41,65 +31,21 @@ export async function generateMetadata({
   };
 }
 
-export default async function GuidePage({ params }: GuidePageProps) {
+export default async function Page({ params }: GuidePageProps) {
   const { lang, group, slug } = params;
 
   const { content, title, prevDocument, nextDocument } =
     await getDocumentBySlug(lang, group, slug);
   const allDocuments = getAllDocuments(lang);
-  const tocItems = generateTOC(content);
 
   return (
-    <DynamicLayout
-      sidebar={
-        <Sidebar
-          documents={allDocuments}
-          currentSlug={slug}
-          currentGroup={group}
-          lang={lang}
-        />
-      }
-      mobileSidebar={
-        <MobileSidebar
-          documents={allDocuments}
-          currentSlug={slug}
-          currentGroup={group}
-          lang={lang}
-        />
-      }
-      toc={<TOC items={tocItems} />}
-    >
-      <div className="p-4 sm:p-8 md:p-12 bg-white dark:bg-black rounded-sm">
-        <p className="text-lg sm:text-xl font-bold mb-4 dark:text-white">
-          {group} &gt; {title}
-        </p>
-        <ReactMarkdown
-          className="prose prose-sm sm:prose lg:prose-lg dark:prose-invert prose-headings:dark:text-white prose-p:dark:text-gray-300 prose-strong:dark:text-white prose-code:dark:text-white prose-ul:dark:text-gray-300 prose-ol:dark:text-gray-300"
-          rehypePlugins={[rehypeRaw, rehypeHighlight]}
-        >
-          {content}
-        </ReactMarkdown>
-      </div>
-      <div className="mt-8 mb-4 flex justify-between gap-4">
-        <div className="flex-1">
-          {prevDocument && (
-            <NavigationButton
-              document={prevDocument}
-              lang={lang}
-              direction="prev"
-            />
-          )}
-        </div>
-        <div className="flex-1">
-          {nextDocument && (
-            <NavigationButton
-              document={nextDocument}
-              lang={lang}
-              direction="next"
-            />
-          )}
-        </div>
-      </div>
-    </DynamicLayout>
+    <GuidePage
+      params={params}
+      content={content}
+      title={title}
+      prevDocument={prevDocument}
+      nextDocument={nextDocument}
+      allDocuments={allDocuments}
+    />
   );
 }
--- packages/docs/lib/generateTOC.ts
+++ packages/docs/lib/generateTOC.ts
@@ -7,10 +7,10 @@ export function generateTOC(content: string): TOCItem[] {
   const lines = content.split("\n");
   const toc: TOCItem[] = [];
 
-  lines.forEach((line, index) => {
+  lines.forEach((line) => {
     if (line.startsWith("<h2>")) {
       const title = line.slice(4, -5).trim();
-      const id = title.toLowerCase().replace(/[^a-z0-9]+/g, "-");
+      const id = title.toLowerCase().replace(/[^a-z0-9가-힣]+/g, "-");
       toc.push({ id, title });
     }
   });
